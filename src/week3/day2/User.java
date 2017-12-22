abdce183f38e91f4c39ef3b66445450003e8b823
olegalas <disposition@ukr.net>
2017-12-22 09:00:15 +0200

    aded week3

---
+++ src/week3/day2/User.java
@@ -0,0 +1,9 @@
+package week3.day2;
+
+public class User {
+
+    String name;
+    int age;
+    String password;
+
+}
---
+++ src/week3/day2/UserTest.java
@@ -0,0 +1,16 @@
+package week3.day2;
+
+public class UserTest {
+
+    public static void main(String[] args) {
+
+
+        User user = new User();
+        System.out.println(user.age);
+
+        user.age = 5;
+        System.out.println(user.age);
+
+    }
+
+}
